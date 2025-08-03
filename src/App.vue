b4b766c7eda8eb392bd917be03ba6dd52ab56eb4
Julien NIAMKE <jniamke@weblogy.com>
2025-08-03 13:54:29 +0000

    finish again

--- src/App.vue
+++ src/App.vue
@@ -466,8 +466,8 @@ export default defineComponent({
         document.head.appendChild(style);
 
         setTimeout(() => {
-          const contentHeight = 280 + ((this.orderResponse?.orderItems.length ?? 1 )  * 50)
-          const desiredHeight = Math.max(200, contentHeight + 20); // Minimum 200mm, ou hauteur du contenu + un peu de marge
+          const contentHeight = 150 + ((this.orderResponse?.orderItems.length ?? 1 )  * 30)
+          const desiredHeight = Math.max(150, contentHeight + 15); // Minimum 200mm, ou hauteur du contenu + un peu de marge
           const opt = {
             margin: [5, 0, 5, 0],
             filename: `Facture_${this.getShortUuid(
